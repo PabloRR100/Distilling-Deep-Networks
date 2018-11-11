b3017180322c566793a8c0295a0f4bc4954895a3
PabloRR100 <pabloruizruiz10@gmail.com>
2018-11-11 17:42:35 -0500

    modularize training and found missing model.eval()

--- 0_SDG_PyTorch.py
+++ 0_SDG_PyTorch.py
@@ -25,8 +25,9 @@ Created on Thu Nov  1 09:47:02 2018
 
 import pickle
 import numpy as np
-from sklearn.datasets import make_moons, load_iris
+from sklearn.datasets import make_moons, make_classification, load_iris, load_digits
 from sklearn.model_selection import train_test_split
+
 
 # DATASETS
 # --------
@@ -34,14 +35,25 @@ from sklearn.model_selection import train_test_split
 from utils import to_df, scatterplot
 
 ## Make moon dataset
-#X, y = make_moons(n_samples=5000, random_state=42, noise=0.1)
-#df = to_df(X, y)
-#df.head()
+X, y = make_moons(n_samples=5000, random_state=42, noise=0.1)
+df = to_df(X, y)
 
-# Iris dataset
-iris = load_iris()
-X = iris.data[:, :2]  # we only take the first two features.
-y = iris.target
+
+## Iris dataset
+#iris = load_iris()
+#X = iris.data[:, :]
+#y = iris.target
+
+
+## NIST Dataset -- 8x8 digits 1000 images
+#nist = load_digits()
+#X = nist.data
+#y = nist.target
+
+
+# Make random classification dataset
+#X, y = make_classification(n_samples=1000, n_features=20, n_informative=2, n_redundant=2, 
+#                    n_repeated=0, n_classes=2, n_clusters_per_class=2, random_state=42)
 
 
 #scatterplot([df])
@@ -50,29 +62,28 @@ y = iris.target
 # Split the data for training and testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-df_train = to_df(X_train, y_train)
-df_test = to_df(X_test, y_test)
-
+#df_train = to_df(X_train, y_train)
+#df_test = to_df(X_test, y_test)
 #scatterplot([df, df_train, df_test], ['Original data', 'Training set', 'Test set'])
 
 
 
-# NEURAL NETWORK
-# --------------
+# NEURAL NETWORK CONFIG
+# ---------------------
 
-inp_dim = 2
+inp_dim = X_train.shape[1]
 n_layers = 2
-lay_size = 3
+lay_size = 10
 n_class = len(np.unique(y_train))
 
 EPOCHS = 100
-BATCHSIZE = 32
+BATCHSIZE = 16
 learning_rate = 0.01
 momentum = 0
 weight_decay = 0
 nesterov = False
 
-pytorch = dict(train_loss = list(), train_accy = list(), 
+results = dict(train_loss = list(), train_accy = list(), 
                valid_loss = list(), valid_accy = list())
 
 
@@ -83,87 +94,42 @@ import torch.optim as optim
 from torch.autograd import Variable
 
 
-# Pass data to PyTorch Dataloader ## PyTorch does not use onehotencoded
+# Define Data Loaders
+# --------------------
+## PyTorch does not use onehotencoded
 from utils import create_torch_dataset
     
 tr_loader = create_torch_dataset(X_train, y_train, BATCHSIZE, shuffle=True)
-ts_loader = create_torch_dataset(X_test, y_test, BS=BATCHSIZE, shuffle=False)
+ts_loader = create_torch_dataset(X_test, y_test, BS=BATCHSIZE, shuffle=True)
 
-    
+
+# Define Network
+# --------------    
+
 from models.fcnn import TorchNet
-#torchnet = TorchNet(inp_dim, n_class, lay_size, n_layers)
-torchnet = TorchNet(inp_dim, n_class, lay_size, n_layers, track_stats=False, recursive=5)
+from train_valid_test import train_epoch, valid_epoch
+
+torchnet = TorchNet(inp_dim, n_class, lay_size, n_layers, track_stats=False, recursive=2)
+
+
+# Define Training
+# ---------------
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(torchnet.parameters(), learning_rate, momentum, weight_decay, nesterov=nesterov)
 
-
-torchnet.train()
-# Training (and validating)
 for epoch in range(EPOCHS):    
-            
-    # Run minibaches from the training dataset
-    for i, (X, labels) in enumerate(tr_loader):
-        
-        X, labels = Variable(X), Variable(labels)
-        
-        # Forward pass
-        torchnet.zero_grad()
-        y_pred = torchnet(X)
-        s, preds = torch.max(y_pred.data, 1)
-        
-        # Compute loss 
-        loss = criterion(y_pred, labels)            
-        
-        # Backward pass
-        loss.backward()
-        optimizer.step()
-        
-        # Collect stats 
-        torchnet.collect_stats(learning_rate)
-
-    # Compute and store epoch results
-    correct, total = 0, 0
-    total += y_pred.size(0)
-    correct += int(sum(preds == labels)) 
-    accuracy = correct / total
     
-    lss = round(loss.item(), 3)
-    acc = round(accuracy * 100, 2)
-
-    pytorch['train_loss'].append(lss)
-    pytorch['train_accy'].append(acc)    
-    
-    print('Epoch {} -- Training: Loss: {}, Accy: {}'
-          .format(epoch, lss, acc))
+    # Training
+    torchnet.train()
+    lss, acc = train_epoch(torchnet, tr_loader, criterion, optimizer, learning_rate, results)
+    print('Epoch {} -- Training: Loss: {}, Accy: {}'.format(epoch, lss, acc))
     
     # Validation
-    correct, total = 0, 0
-    for i, (X, labels) in enumerate(ts_loader):
-        
-        X, labels = Variable(X), Variable(labels)
-        
-        # Forward pass
-        y_pred = torchnet(X)
-        s, preds = torch.max(y_pred.data, 1)
-        
-        # Compute loss 
-        loss = criterion(y_pred, labels)           
-        
-        # Compute and store epoch results
-        total += y_pred.size(0)
-        correct += int(sum(preds == labels)) 
-        
-    accuracy = correct / total
+    torchnet.eval()
     
-    lss = round(loss.item(), 3)
-    acc = round(accuracy * 100, 2)
-
-    pytorch['valid_loss'].append(lss)
-    pytorch['valid_accy'].append(acc)
-        
-    print('Epoch {} -- Validation: Loss: {}, Accy: {}'
-          .format(epoch, lss, acc))
+    lss, acc = valid_epoch(torchnet, ts_loader, criterion)    
+    print('Epoch {} -- Validation: Loss: {}, Accy: {}'.format(epoch, lss, acc))
 
 
 # torch.save(torchnet, 'torchnet.pkl')
@@ -180,16 +146,17 @@ from sklearn.metrics import confusion_matrix
 # Performance Analysis
 # ---------------------
 
-# Targets vs Predictions  
-
-from utils import true_vs_pred
 y_pred_all = torchnet(Variable(torch.tensor(X_test, dtype=torch.float32)))
 y_pred_all = torch.max(y_pred_all.data, 1)[1].detach().numpy()
-
-df_test = to_df(X_test, y_test)
-df_pred = to_df(X_test, y_pred_all)
-
-true_vs_pred(df_test, df_pred)
+    
+# Targets vs Predictions (Only for 2D Inputs)
+if X_train.shape[1] == 2:
+    
+    from utils import true_vs_pred
+    df_test = to_df(X_test, y_test)
+    df_pred = to_df(X_test, y_pred_all)
+    
+    true_vs_pred(df_test, df_pred)
 
 # Confusion Matrix
 confusion_matrix(y_test, y_pred_all)
@@ -197,10 +164,10 @@ confusion_matrix(y_test, y_pred_all)
 
 # Train Validation Loss Accuracy
 fig, axs = plt.subplots(nrows=2, ncols=1)
-sns.lineplot(range(EPOCHS), pytorch['train_loss'], label='Training', ax=axs[0])
-sns.lineplot(range(EPOCHS), pytorch['valid_loss'], label='Validation', ax=axs[0])
-sns.lineplot(range(EPOCHS), pytorch['train_accy'], label='Training', ax=axs[1])
-sns.lineplot(range(EPOCHS), pytorch['valid_accy'], label='Validation', ax=axs[1])
+sns.lineplot(range(EPOCHS), results['train_loss'], label='Training', ax=axs[0])
+sns.lineplot(range(EPOCHS), results['valid_loss'], label='Validation', ax=axs[0])
+sns.lineplot(range(EPOCHS), results['train_accy'], label='Training', ax=axs[1])
+sns.lineplot(range(EPOCHS), results['valid_accy'], label='Validation', ax=axs[1])
 axs[0].set_title('Loss')
 axs[1].set_title('Accuracy')
 plt.plot()
